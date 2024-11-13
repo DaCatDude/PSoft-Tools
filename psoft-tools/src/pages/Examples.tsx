707e62e45a25160c375179bafd5d5c771b06df62
caitlinyau0717 <yauc2@rpi.edu>
2024-11-12 22:57:51 -0500

    Update Examples.tsx
    Added second method example and information about it.

--- psoft-tools/src/pages/Examples.tsx
+++ psoft-tools/src/pages/Examples.tsx
@@ -5,7 +5,15 @@ export default function Examples() {
     more := x + y;
     less := x - y;
     // comments: are not strictly necessary.
-}`;
+    }`;
+    const code2 = `method Abs(x: int) returns (y: int)
+    {
+    if x < 0 {
+        return -x;
+    } else {
+        return x;
+    }
+    }`;
 
     return (
         <div style={{ padding: "20px", marginTop: "60px", textAlign: "left" }}>
@@ -18,7 +26,7 @@ export default function Examples() {
                 link
             </a> to learn Dafny and by making this page, I learned some TypeScript.</p>
             
-            <h2 style={{ marginTop: "30px" }}>Dafny Method Example:</h2>
+            <h2 style={{ marginTop: "30px" }}>Dafny Method Example #1:</h2>
             <pre style={{
                 backgroundColor: "#f5f5f5",
                 padding: "10px",
@@ -35,6 +43,22 @@ export default function Examples() {
             <p style={{ marginTop: "10px" }}>I additionally learned that functions exist in Dafny but serve a different purpose.</p>
             <p style={{ marginTop: "10px" }}>Assignments use := and Equality uses ==</p>
             <p style={{ marginTop: "10px" }}>Single line comments are made using // and Multi-line comments can be made using /**/</p>
+
+            <h2 style={{ marginTop: "30px" }}>Dafny Method Example #2:</h2>
+            <pre style={{
+                backgroundColor: "#f5f5f5",
+                padding: "10px",
+                borderRadius: "5px",
+                fontFamily: "monospace",
+                overflowX: "auto",
+                whiteSpace: "pre-wrap",
+                border: "1px solid #ddd"
+            }}>
+                <code>{code2}</code>
+            </pre>
+
+            <p style={{ marginTop: "10px" }}>This portion of code is another method example called Abs and takes in one input x which is an int and returns a value y which is also an int. Inside the method, there is an if statement that returns -x if the value of x is less than 0 or else it returns x. This method ultimately takes the absolute value of x and returns it.</p>
+            <p style={{ marginTop: "10px" }}>This function displays the use of return statements and if statements in Dafny.</p>
         </div>
     );
 }
