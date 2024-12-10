c4cb01da0d9d1124bc3f9b1013a9324fa9ed769e
Eshan Ilahi <ilahi.eshan@gmail.com>
2024-12-10 18:31:57 -0500

    made java class to receive json object and convert contents into a java object

---
+++ psoft-tools/src/pages/JSONParsing.java
@@ -0,0 +1,20 @@
+public class JSONParsing {
+    private String filename;
+    private String code;
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
